629f5ae9630ec2f99f98e811eafcfea6f4663725
AmirAliEidivandi <amirah648887@gmail.com>
2022-06-14 20:46:09 +0430

    validate login form

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,22 @@
+import "./App.css";
+import { ToastContainer } from "react-toastify";
+import { Routes, Route } from "react-router-dom";
+import "react-toastify/dist/ReactToastify.css";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+
 function App() {
-    return <h1>hello world</h1>;
+    return (
+        <div className="App">
+            <ToastContainer />
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/login" element={<Login />} />
+                <Route path="/register" element={<Register />} />
+            </Routes>
+        </div>
+    );
 }
 
 export default App;
