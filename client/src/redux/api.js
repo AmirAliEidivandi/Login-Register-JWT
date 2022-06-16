6c17b21c3e25f4c51e8002918329b03be2226b21
AmirAliEidivandi <amirah648887@gmail.com>
2022-06-16 21:37:28 +0430

    complete login and register

--- client/src/redux/api.js
+++ client/src/redux/api.js
@@ -4,3 +4,4 @@ const API = axios.create({ baseURL: "http://localhost:5000" });
 
 export const signIn = (formData) => API.post("/auth/signin", formData);
 export const signUp = (formData) => API.post("/auth/signup", formData);
+export const googleSignIn = (result) => API.post("/auth/googleSignin", result);
