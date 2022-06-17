78865beac838180a7166cd6e4f91993a1f1ece85
AmirAliEidivandi <amirah648887@gmail.com>
2022-06-17 21:21:23 +0430

    complete login and logout

--- client/src/App.js
+++ client/src/App.js
@@ -1,3 +1,4 @@
+import { useEffect } from "react";
 import "./App.css";
 import { ToastContainer } from "react-toastify";
 import { Routes, Route } from "react-router-dom";
@@ -5,10 +6,21 @@ import "react-toastify/dist/ReactToastify.css";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
+import Header from "./components/Header";
+import { useDispatch } from "react-redux";
+import { setUser } from "./redux/features/authSlice";
 
 function App() {
+    const dispatch = useDispatch();
+    const user = JSON.parse(localStorage.getItem("profile"));
+
+    useEffect(() => {
+        dispatch(setUser(user));
+    }, []);
+
     return (
         <div className="App">
+            <Header />
             <ToastContainer />
             <Routes>
                 <Route path="/" element={<Home />} />
--- client/src/redux/features/authSlice.js
+++ client/src/redux/features/authSlice.js
@@ -41,6 +41,15 @@ const authSlice = createSlice({
         error: "",
         loading: false,
     },
+    reducers: {
+        setUser: (state, action) => {
+            state.user = action.payload;
+        },
+        setLogout: (state, action) => {
+            localStorage.clear();
+            state.user = null;
+        },
+    },
     extraReducers: {
         [login.pending]: (state, action) => {
             state.loading = true;
@@ -80,5 +89,7 @@ const authSlice = createSlice({
         },
     },
 });
+
+export const { setUser, setLogout } = authSlice.actions;
 
 export default authSlice.reducer;
